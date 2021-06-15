87c5f8f434f7d592264d46cf999c2df763a60589
JeffersonOmarDiaz <jefferson.diaz@epn.edu.ec>
2021-06-14 21:20:12 -0500

    Logica captura de datos mascotas seleccionadas para el paseo

--- src/app/pages/pet-paseo/pet-paseo.component.ts
+++ src/app/pages/pet-paseo/pet-paseo.component.ts
@@ -31,6 +31,23 @@ export class PetPaseoComponent implements OnInit {
   };
   
   clienteMascota : Mascota []=[];
+  //INICIO Para capturar los datos de notificación de mascota
+  clientNotifi: Cliente = {
+    uid: this.uid,
+    email: '',
+    celular: '',
+    foto: '',
+    referncia: '',
+    ubicacion: null,
+    edad: null,
+    nombre: '',
+    apellido: '',
+    cedula: '',
+    mascotas: [],
+  };
+  mascotaNotifi: Mascota []=[];
+  mostrarDialogo: boolean;
+  //FIN Para capturar los datos de notificación de mascota
   constructor(public firebaseauthS: FirebaseauthService,
               public firestoreService: FirestoreService,) { 
     this.firebaseauthS.stateAuth().subscribe( res => {
@@ -56,5 +73,35 @@ export class PetPaseoComponent implements OnInit {
       console.log('La informacion de las mascotas cliente es: ', this.clienteMascota);
     });
     return;
+  }
+  
+  estado(checked: any, informacion: any){
+    console.log('Esta desmarcado: ',checked.currentTarget.checked);
+    if(!checked.currentTarget.checked){
+      //console.log('Los datos de que se cargaran es: ',informacion);
+      this.mascotaNotifi = informacion;
+      this.clientNotifi.mascotas.push(informacion);
+    }else if(checked.currentTarget.checked){
+      let idMascota = '';
+      let posicionArray = null;
+      const recorreArray = (arr) => {
+        for(let i=0; i<=arr.length-1; i++){
+          idMascota = arr[i].id;
+          if(arr[i].id === informacion.id){
+            posicionArray = i;
+          return;
+        }
+        }
+      }
+      recorreArray(this.clientNotifi.mascotas);
+      this.clientNotifi.mascotas.splice(posicionArray, 1);
+      //console.log(this.clientNotifi.mascotas);
+    }
+    console.log('La información para la notificación es: ',this.clientNotifi);
+    if(this.clientNotifi.mascotas.length === 0){
+      this.mostrarDialogo = false;
+    }else{
+      this.mostrarDialogo = true;
+    }
   }
 }
